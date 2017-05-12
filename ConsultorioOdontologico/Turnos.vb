e6ccbe85551a3af4d72bab9176eb14e41629082d
LucianoRigourd <lucianorigourd@gmail.com>
2017-05-12 00:22:50 -0300

    Turnos: pequeños cambios

--- ConsultorioOdontologico/Turnos.vb
+++ ConsultorioOdontologico/Turnos.vb
@@ -5,16 +5,6 @@
 
     Private Sub frm_turnos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'Asigna la cadena de conexion segun la compu actual (por el tema de Git y eso)
-        Select Case Environment.UserName
-            Case "Luciano" 'Lucho
-                cadena_conexion = "Provider=SQLNCLI11;Data Source=LAPATOP\LUCIANOSQL;User ID=Luciano;Initial Catalog=ConsultorioOdontologicoBD;Password=Luciano"
-            Case "NOTEBOOK" 'maxeeeh
-                cadena_conexion = "Provider=SQLNCLI11;Data Source=BGH\MORILLASSQL;User ID=Morillas;Initial Catalog=ConsultorioOdontologicoBD;password=Morillas"
-        End Select
-
-
-
     End Sub
 
 
