ca8fdc0e00016b1c63ba5398e1bcd6f663845b83
maxeeeh <maxidmorillas@gmail.com>
2017-05-29 18:52:35 -0300

    Factura: agregado lo de la rama de leo

---
+++ ConsultorioOdontologico/Factura.designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Factura
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmb_pacientes = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmb_intervenciones = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmb_pacientes
+        '
+        Me.cmb_pacientes.FormattingEnabled = True
+        Me.cmb_pacientes.Location = New System.Drawing.Point(114, 15)
+        Me.cmb_pacientes.Name = "cmb_pacientes"
+        Me.cmb_pacientes.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_pacientes.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Pacientes"
+        '
+        'cmb_intervenciones
+        '
+        Me.cmb_intervenciones.FormattingEnabled = True
+        Me.cmb_intervenciones.Location = New System.Drawing.Point(114, 86)
+        Me.cmb_intervenciones.Name = "cmb_intervenciones"
+        Me.cmb_intervenciones.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_intervenciones.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Intervenciones "
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(19, 168)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(537, 132)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'Factura
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(614, 312)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmb_intervenciones)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmb_pacientes)
+        Me.Name = "Factura"
+        Me.Text = "Factura"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmb_pacientes As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmb_intervenciones As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+End Class
---
+++ ConsultorioOdontologico/Factura.vb
@@ -0,0 +1,28 @@
+﻿Public Class Factura
+    Dim clase_auxiliar As New Atributos_Compartidos
+    Private Sub Factura_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        clase_auxiliar.cargar_combobox(cmb_pacientes, tabla_para_combo("Paciente"))
+
+    End Sub
+
+   
+
+    Private Function tabla_para_combo(ByVal nombre_tabla As String) As DataTable ' el parametro puede ser "Empleado" o "Paciente"
+        'Esta funcion da una tabla con 2 columnas: la primera es el id_paciente o id_empleado, y la segunda el apellido y nombre concatenados
+
+        Dim tabla As New Data.DataTable
+        Dim sql As String = ""
+        'En el select hace id_empleado o id_paciente, y despues concatena asi: "Apellido, Nombre"
+        sql &= "SELECT id_" & nombre_tabla.ToLower() & ", apellido + ', ' + nombre"
+        sql &= " FROM " & nombre_tabla
+        sql &= " WHERE habilitado = 1"
+        sql &= " ORDER BY apellido"
+        tabla = clase_auxiliar.ejecuto_sql(sql)
+
+       
+
+        Return tabla
+
+    End Function
+End Class
