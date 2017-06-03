e56925a188b13140eabb7a7a30b0289323f32ec3
maxeeeh <maxidmorillas@gmail.com>
2017-06-03 15:42:01 -0300

    errores

--- ConsultorioOdontologico/My Project/Application.Designer.vb
+++ ConsultorioOdontologico/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ConsultorioOdontologico.Turnos_Report
+            Me.MainForm = Global.ConsultorioOdontologico.Insumo_Reporrt
         End Sub
     End Class
 End Namespace
